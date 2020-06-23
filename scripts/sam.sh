0e725e33d9b815e748f9a43fb45529f0e949b522
jeshan <j@jeshan.co>
2020-06-24 02:10:19 +0400

    mv stack name to .env

--- scripts/sam.sh
+++ scripts/sam.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 bucket=`python scripts/get_bucket.py`
-sam deploy --s3-bucket ${bucket} --region ${AWS_DEFAULT_REGION}
+sam deploy --s3-bucket ${bucket} --region ${AWS_DEFAULT_REGION} --stack-name ${STACK_NAME}
