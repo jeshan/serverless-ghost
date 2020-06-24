8739513a0df78986be6ba6021e944449627897e9
jeshan <j@jeshan.co>
2020-06-24 23:20:21 +0400

    fix release script to deploy with correct permissions

--- scripts/sam-release.sh
+++ scripts/sam-release.sh
@@ -5,5 +5,5 @@ bucket=jeshan-oss-public-files
 prefix=serverless-ghost
 output_file=template-packaged.yaml
 
-sam package --s3-bucket ${bucket} --region us-east-1 --s3-prefix ${prefix} --output-template-file ${output_file}
-aws s3 cp ${output_file} s3://${bucket}/${prefix}/template.yaml
+sam package --profile jeshanco --s3-bucket ${bucket} --region us-east-1 --s3-prefix ${prefix} --output-template-file ${output_file}
+aws s3 cp --profile jeshanco ${output_file} s3://${bucket}/${prefix}/template.yaml
